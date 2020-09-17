01ae094a39c566de09ee32a002db50aa549884d1
smektor <wiktor.jankowski13@gmail.com>
2020-09-17 23:43:37 +0200

    delete docker folder

--- docker_rails_env/README.md
+++
@@ -1,8 +0,0 @@
-# Example docker development environment for RubyOnRails
-
-## Used images:
-###   web
-Rails app.
-
-###   db
-    image: postgres:12.3-alpine
--- docker_rails_env/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
-# Check for new gems if there are install them
-bundle check || bundle install
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
